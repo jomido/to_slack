74d6a7f169c31016194278c28bd55dbc2f55d4ef
Jonathan Dobson <jon.m.dobson@gmail.com>
2017-10-16 14:28:24 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,84 @@
+
+# to_slack
+
+Send messages to Slack from Python. You'll need to [get hooks from Slack](https://api.slack.com/tutorials/slack-apps-hello-world) for each user or channel. Each hook is a string. They look like this:
+
+> https://hooks.slack.com/services/X03SU38LS/B7JT9RT6D/rRSoP986AkaZ5capZyyXOgJx
+
+You just need the part after `https://hooks.slack.com/services/`. So, for the above hook, just `X03SU38LS/B7JT9RT6D/rRSoP986AkaZ5capZyyXOgJx`.
+
+```python
+from to_slack import To
+
+to = To({
+    'sally': '<hook>',
+    'david': '<hook>',
+    'general': '<hook>'
+})
+
+# send a message to sally
+to.sally('Hello')
+
+# send a message to sally and david
+to.sally.david('Hello')
+
+# send a message to sally and david and the general channel
+to.sally.david.general('Hello')
+
+# etc.
+
+```
+
+You specify each target for the message via dot notation, after the `To` instance. Targets are flushed after a message is sent, but you can freeze them:
+
+```python
+
+from to_slack import freeze
+
+to_peeps = freeze(to.sally.david)
+
+to_peeps('hey')
+to_peeps('grass')
+```
+
+## wait, I have a '.' in my username
+
+Sure:
+
+```python
+from to_slack import To
+
+to = To({
+    'no.regrets': '<hook>'
+})
+
+# send a message to no.regrets
+to.no.regrets('Hello')
+
+# this will fail
+to.no('Hello')  # Exception: Invalid target: "no".
+
+# so will this
+to.no.regets.man('Hello')  # Exception: Invalid target: "man".
+```
+
+## wait, I have a '-' in my username
+
+That's more problematic, but ok:
+
+```python
+from to_slack import To
+
+to = To({
+    'no.regrets': '<hook>',
+    'sam': '<hook>',
+    'so-hyphen': '<hook>'
+})
+
+to['so-hyphen'].sam.no.regrets('hi all')
+```
+
+## installation
+
+ * copy this file wherever you like (sorry, no PyPI)
+ * install requests, or `pip install -r requirements.txt`
---
+++ to_slack.py
@@ -0,0 +1,121 @@
+
+import logging
+
+from functools import partial
+
+import requests
+
+_log = logging.getLogger(__name__)
+_log.setLevel(logging.DEBUG)
+
+
+class To:
+
+    def __init__(self, hooks=None):
+
+        self._root = 'https://hooks.slack.com/services'
+        self._hooks = hooks or {}
+        self._targets = []
+        self._partial = []
+
+        self._verify()
+
+    def __call__(self, msg):
+
+        if self._partial:
+            raise Exception('Invalid target: "{}".'.format(
+                '.'.join(self._partial)
+            ))
+
+        targets = self._flush()
+        self._send_all(targets, msg)
+
+    def __getattr__(self, attr):
+
+        if self._partial:
+            self._partial.append(attr)
+            attr = '.'.join(self._partial)
+
+        if attr not in self._hooks:
+
+            if not self._partial:
+                self._partial.append(attr)
+
+            return self
+
+        self._targets.append(
+            '{}/{}'.format(self._root, self._hooks[attr])
+        )
+
+        self._partial = []
+
+        return self
+
+    def __getitem__(self, item):
+
+        return getattr(self, item)
+
+    def _verify(self):
+
+        for k, v in self._hooks.items():
+            if v.startswith('https://'):
+                raise Exception('Invalid hook for "{}": "{}".'.format(
+                    k, v
+                ))
+
+    @classmethod
+    def _send_all(cls, channels, msg):
+
+        for chan in channels:
+            try:
+                cls._send(chan, msg)
+            except Exception as ex:
+                print('Could not send message to "{}": {}'.format(chan, ex))
+
+    def _flush(self):
+
+        targets = set(self._targets[:])
+        self._targets = []
+
+        return targets
+
+    @classmethod
+    def _send(cls, chan, msg):
+
+        _log.debug('Sending "{}" to "{}"...'.format(
+            msg, chan
+        ))
+        r = requests.post(
+            chan,
+            json={"text": msg}
+        )
+
+        if r.status_code != 200:
+
+            try:
+                msg = r.json()
+            except:
+                msg = r.text
+
+            raise Exception(msg)
+
+
+def freeze(to):
+
+    targets = to._flush()
+
+    return partial(
+        To._send_all,
+        targets
+    )
+
+
+if __name__ == "__main__":
+
+    hook = 'T03SU38LS/B7JC9RT6D/rRSoP9C6AkaZ5capZqyXOgJx'
+
+    to = To({
+        'root': hook
+    })
+
+    to.root('ha!')
